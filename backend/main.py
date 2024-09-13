ec3d48ac2eb171afb4b82f46d18dd2f49742a1e8
justinlzx <justinlee01.jl@gmail.com>
2024-09-13 18:46:40 +0800

    add: connected frontend to backend

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,24 @@ from auth.routes import router as auth_router
 from users.routes import router as users_router
 from database import engine
 from auth.models import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+origins = [
+    "http://localhost:3000",
+]
+
+# middlwares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Include the auth and user routes
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
