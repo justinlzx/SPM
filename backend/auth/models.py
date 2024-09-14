a05103dbab3d08e698046d6f447f10402f47e5aa
tanhaoen <nathanieltan1701@gmail.com>
2024-09-14 10:16:28 +0800

    Revert "refactor: structure code into folders based on entity or service"
    This reverts commit 1679f9110c5be9b2d41d11e9e6bfe04170518d67.

---
+++ backend/auth/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    role = Column(String)
+    
+def create_user(db, uuid: str, email: str, username: str, hashed_password: str, role: str):
+    new_user = User(uuid=uuid, email=email, username=username, hashed_password=hashed_password, role=role)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def get_user_by_username(db, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+def get_user_by_email(db, email: str):
+    return db.query(User).filter(User.email == email).first()
