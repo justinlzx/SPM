bf2dfc6b39f4e3f43dfc4f6bc9a99c8834b3b892
tanhaoen <nathanieltan1701@gmail.com>
2024-09-14 09:52:31 +0800

    refactor: separate FastAPI start app logic

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,5 @@
-from fastapi import FastAPI
-from auth.routes import router as auth_router
-from users.routes import router as users_router
-from database import engine
-from auth.models import Base
-from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from src.app import app
 
-app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
-
-origins = [
-    "http://localhost:3000",
-]
-
-# middlwares
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,  # Allow specific origins
-    allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # Allow all headers
-)
-
-# Include the auth and user routes
-app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
-app.include_router(users_router, prefix="/users", tags=["Users"])
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ backend/src/app.py
@@ -0,0 +1,39 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from .employees import models as employee_models
+
+from .auth.routes import router as auth_router
+
+from .users.routes import router as users_router
+
+from .database import engine
+
+from .init_db import load_data
+
+employee_models.Base.metadata.create_all(bind=engine)
+
+"""
+Create a context manager to handle the lifespan of the FastAPI application
+Code before the yield keyword is run before the application starts
+Code after the yield keyword is run after the application stops
+"""
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Recreate all tables
+    employee_models.Base.metadata.create_all(bind=engine)
+    
+    # Load employee data from CSV
+    load_data.load_employee_data_from_csv("./src/init_db/employee.csv")
+    
+    yield
+    
+    # Drop all tables
+    employee_models.Base.metadata.drop_all(bind=engine)
+
+app = FastAPI(lifespan=lifespan)
+
+# Include the auth and user routes
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(users_router, prefix="/users", tags=["Users"])
