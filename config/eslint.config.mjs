dc451c6b0c66c39063f0b45c2c10170e23bae87d
tanhaoen <nathanieltan1701@gmail.com>
2024-09-20 23:14:44 +0800

    build: add support for typescript linting

--- config/eslint.config.mjs
+++ config/eslint.config.mjs
@@ -1,11 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import pluginTypeScript from "@typescript-eslint/eslint-plugin";
+import parserTypeScript from "@typescript-eslint/parser";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: parserTypeScript,
+    },
+    plugins: {
+      "@typescript-eslint": pluginTypeScript,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      ...pluginTypeScript.configs.recommended.rules,
+    }
+  
+  }
 ];
