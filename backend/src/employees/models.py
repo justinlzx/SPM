d0760085948254942d4ab6717cd4d3d4f44f7532
justinlzx <justinlee01.jl@gmail.com>
2024-09-21 18:08:13 +0800

    add: test for recursive FK for employee schema

--- backend/src/employees/models.py
+++ backend/src/employees/models.py
@@ -1,6 +1,8 @@
+from sqlalchemy.orm import Session
 from sqlalchemy import CheckConstraint, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
+from ..database import engine
 from ..database import Base
 
 
@@ -18,3 +20,60 @@ class Employee(Base):
     role = Column(Integer, CheckConstraint("role IN (1, 2, 3)"), nullable=False)
 
     manager = relationship("Employee", remote_side=[staff_id])
+
+
+if __name__ == "__main__":
+
+    session = Session(bind=engine)
+
+    director = Employee(
+        staff_id=3,
+        staff_fname="Johnson",
+        staff_lname="Doe",
+        email="johnson.doe@test.com",
+        dept="IT",
+        position="Director",
+        country="USA",
+        role=2,
+    )
+
+    session.add(director)
+    session.commit()
+
+    manager = Employee(
+        staff_id=1,
+        staff_fname="Jane",
+        staff_lname="Smith",
+        email="jane.smith@test.com",
+        dept="IT",
+        position="Manager",
+        country="USA",
+        role=2,
+        reporting_manager=3,
+    )
+
+    session.add(manager)
+    session.commit()
+
+    employee = Employee(
+        staff_fname="John",
+        staff_lname="Doe",
+        email="john.doe@test.com",
+        dept="IT",
+        position="Manager",
+        country="USA",
+        reporting_manager=1,
+        role=3,
+    )
+
+    session.add(employee)
+    session.commit()
+
+    session.refresh(employee)
+
+    # Print the manager object
+    print(employee.manager.staff_fname)
+    print(manager.manager.staff_fname)
+
+    # Close the session
+    session.close()
