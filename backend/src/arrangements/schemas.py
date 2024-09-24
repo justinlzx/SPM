12e1929363779bc9de42b1e4096db0915a5bf358
tanhaoen <nathanieltan1701@gmail.com>
2024-09-24 12:17:29 +0800

    feat: add support for creating recurring wfh arrangements

--- backend/src/arrangements/schemas.py
+++ backend/src/arrangements/schemas.py
@@ -8,17 +8,41 @@ from ..base import BaseSchema
 
 
 class ArrangementBase(BaseSchema):
-    requester_staff_id: int
-    wfh_date: str
-    wfh_type: Literal["full", "am", "pm"]
+    requester_staff_id: int = Field(
+        ..., title="Staff ID of the employee who made the request"
+    )
+    wfh_date: str = Field(
+        ...,
+        title="Date of an adhoc WFH request or the start date of a recurring WFH request",
+    )
+    wfh_type: Literal["full", "am", "pm"] = Field(..., title="Type of WFH arrangement")
 
 
 class ArrangementCreate(ArrangementBase):
     update_datetime: SkipJsonSchema[datetime] = Field(
-        default_factory=datetime.now, exclude=True
+        default_factory=datetime.now,
+        exclude=True,
+        title="Datetime that the request was created",
     )
-    approval_status: SkipJsonSchema[str] = Field(default="pending", exclude=True)
-    reason_description: str
+    approval_status: SkipJsonSchema[str] = Field(
+        default="pending", exclude=True, title="Approval status of the request"
+    )
+    reason_description: str = Field(..., title="Reason for requesting the WFH")
+    is_recurring: bool = Field(
+        default=False, title="Flag to indicate if the request is recurring"
+    )
+    recurring_end_date: str = Field(
+        default=None, title="End date of a recurring WFH request"
+    )
+    recurring_frequency_number: int = Field(
+        default=None, title="Numerical frequency of the recurring WFH request"
+    )
+    recurring_frequency_unit: Literal["week", "month"] = Field(
+        default=None, title="Unit of the frequency of the recurring WFH request"
+    )
+    recurring_occurrences: int = Field(
+        default=None, title="Number of occurrences of the recurring WFH request"
+    )
 
     def model_dump(self, **kwargs):
         data = super().model_dump(**kwargs)
