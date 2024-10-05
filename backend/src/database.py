ed8bf30332fcfb794a08a02d64ca6ec2e622ee89
zaramichaela <46987186+zaramichaela@users.noreply.github.com>
2024-10-05 22:18:23 +0800

    deleted unnecessary comments

--- backend/src/database.py
+++ backend/src/database.py
@@ -21,32 +21,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
-
-# db = None
-
-
-# class Database:
-#     def __init__(self):
-#         self.engine = create_engine(
-#             SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-#         )
-#         self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
-#         self.db = self.SessionLocal()
-#         self.Base = declarative_base()
-
-#     def get_db(self):
-#         try:
-#             yield self.db
-#         finally:
-#             self.db.close()
-
-
-#     def get_Base(self):
-#         return self.Base
