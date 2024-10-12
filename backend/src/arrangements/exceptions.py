db4f5d1d22e280035d67f168798aef7063fb41e4
zaramichaela <46987186+zaramichaela@users.noreply.github.com>
2024-10-12 15:29:39 +0800

    achieved 100% coverage for arrangements exceptions

--- backend/src/arrangements/exceptions.py
+++ backend/src/arrangements/exceptions.py
@@ -7,6 +7,6 @@ class ArrangementNotFoundError(Exception):
 class ArrangementActionNotAllowedError(Exception):
     def __init__(self, arrangement_id: int, action: str):
         self.message = "Default message"
-        if action == "approve" or "reject":
+        if action == "approve" or action == "reject":
             self.message = f"Arrangement with ID {arrangement_id} is not in pending status"
         super().__init__(self.message)
