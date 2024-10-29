49a1d80e0345244e951d48e4c40ffc6d1b5ee863
tanhaoen <nathanieltan1701@gmail.com>
2024-10-29 22:11:40 +0800

    refactor: move arrangements commons into commons folder

--- backend/src/arrangements/commons/exceptions.py
+++ backend/src/arrangements/commons/exceptions.py
@@ -1,3 +1,6 @@
+from .enums import Action, ApprovalStatus
+
+
 class ArrangementNotFoundException(Exception):
     def __init__(self, arrangement_id: int):
         self.message = f"Arrangement with ID {arrangement_id} not found"
@@ -5,10 +8,8 @@ class ArrangementNotFoundException(Exception):
 
 
 class ArrangementActionNotAllowedException(Exception):
-    def __init__(self, arrangement_id: int, action: str):
-        self.message = "Default message"
-        if action == "approve" or action == "reject":
-            self.message = f"Arrangement with ID {arrangement_id} is not in pending status"
+    def __init__(self, current_approval_status: ApprovalStatus, action: Action):
+        self.message = f"Action {action} not allowed for current status {current_approval_status}"
         super().__init__(self.message)
 
 
