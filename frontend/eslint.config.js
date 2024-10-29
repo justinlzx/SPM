648863c493e48f5ff14abbd41b3e67b2cb8fe698
joshuasoh <joshua.soh.2022@scis.smu.edu.sg>
2024-10-29 16:23:35 +0800

    CI Frontend fix for lint, added prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,34 +1,44 @@
 import js from "@eslint/js";
 import globals from "globals";
-import typescript from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import reactPlugin from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import react from "eslint-plugin-react";
+import * as reactHooks from "eslint-plugin-react-hooks";
 
 export default [
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
-      ecmaVersion: "latest",
+      ecmaVersion: 2024,
       sourceType: "module",
-      parser: tsParser,
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
       globals: {
         ...globals.browser,
         ...globals.es2021
       }
     },
     plugins: {
-      "@typescript-eslint": typescript,
-      "react": reactPlugin,
-      "react-hooks": reactHooksPlugin
+      "@typescript-eslint": typescriptPlugin,
+      "react": react,
+      "react-hooks": reactHooks
     },
     rules: {
       ...js.configs.recommended.rules,
-      ...typescript.configs.recommended.rules,
+      ...typescriptPlugin.configs.recommended.rules,
       "react/jsx-uses-react": "error",
       "react/jsx-uses-vars": "error",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn"
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
     }
   }
 ];
