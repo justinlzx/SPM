8d5131e7ad312b7ede705ddbf3184d6089bde286
jezeleii <jmanangan.2022@scis.smu.edu.sg>
2024-10-30 09:52:26 +0800

    add utils

--- frontend/src/hooks/arrangement/arrangement.utils.ts
+++ frontend/src/hooks/arrangement/arrangement.utils.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { TWFHRequest, ApprovalStatus } from "../../types/requests";
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
@@ -16,46 +17,73 @@ export type TRequest = {
 export type TArrangementQuery = {
   id: number;
   status: string;
-  page: number
-  rowsPerPage: number
-  searchTerm: string
-}
-
-export const getArrangementsByManager = async ({
-    id: manager_id,
-    status,
-    page,
-    rowsPerPage,
-    searchTerm
-}: TArrangementQuery):  Promise<TRequest[] | undefined> => {
-    try {
-        const response = await axios.get(`${BACKEND_URL}/arrangements/subordinates/${manager_id}`, {
-            params: {
-                status,
-                page,
-                rowsPerPage,
-                searchTerm
-            },
-        });
-        if (response.status === 200) {
-            return response.data as TRequest[]
-        }
-    } catch (error: any) {
-        return;
-    }
+  page: number;
+  rowsPerPage: number;
+  searchTerm: string;
 };
 
-export const updateArrangement = async (request: "approve" | "reject", arrangement_id: number): Promise<any| undefined>  => {
-    // try {
-        //TODO: double check endpoint
-        // const response = await axios.put(`${BACKEND_URL}/arrangement/update`, request);
-        // if (response.status === 200) {
-        //     return response.data;
-        // }
-        console.log("updateArrangement", request);
-        return true
-    // } 
-    // catch (error: any) {
-    //     return
-    // }
+export const getArrangementsByManager = async ({
+  id: manager_id,
+  status,
+  page,
+  rowsPerPage,
+  searchTerm,
+}: TArrangementQuery): Promise<TRequest[] | undefined> => {
+  try {
+    const response = await axios.get(
+      `${BACKEND_URL}/arrangements/subordinates/${manager_id}`,
+      {
+        params: {
+          status,
+          page,
+          rowsPerPage,
+          searchTerm,
+        },
+      }
+    );
+    if (response.status === 200) {
+      return response.data as TRequest[];
+    }
+  } catch (error: any) {
+    return;
+  }
+};
+
+export const updateArrangement = async (
+  request: "approve" | "reject",
+  arrangement_id: number
+): Promise<any | undefined> => {
+  // try {
+  //TODO: double check endpoint
+  // const response = await axios.put(`${BACKEND_URL}/arrangement/update`, request);
+  // if (response.status === 200) {
+  //     return response.data;
+  // }
+  console.log("updateArrangement", request);
+  return true;
+  // }
+  // catch (error: any) {
+  //     return
+  // }
+};
+
+export const getPersonalRequests = async (
+  userId: string
+): Promise<TWFHRequest[]> => {
+  try {
+    const response = await axios.get(
+      `${BACKEND_URL}/arrangements/personal/${userId}`
+    );
+
+    // Map and transform the data as needed
+    return response.data.data.map((request: any) => ({
+      ...request,
+      approval_status:
+        ApprovalStatus[request.approval_status as keyof typeof ApprovalStatus],
+      wfh_date: new Date(request.wfh_date), // Convert wfh_date to Date object
+    }));
+  } catch (error) {
+    console.error("Failed to fetch personal WFH requests:", error);
+    throw error; // Rethrow to handle it in the component
+  }
 };
