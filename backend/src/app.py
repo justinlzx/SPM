2b68ddeef65855d4b12773f13cc875546510417f
tanhaoen <nathanieltan1701@gmail.com>
2024-11-08 23:17:30 +0800

    TEST DEPLOYMENT

--- backend/src/app.py
+++ backend/src/app.py
@@ -99,7 +99,7 @@ app.add_middleware(
     allow_headers=["*"],
     expose_headers=["*"],
 )
-
+# Change some code to test
 
 # Include the auth and user routes
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
