b6c1920a3216cfde9e3331834b49947f383ee35f
tanhaoen <nathanieltan1701@gmail.com>
2024-11-08 18:43:31 +0800

    testP: add viewport settings to cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    frontend_url: "http://localhost:3000",
+    backend_url: "http://localhost:8000",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
